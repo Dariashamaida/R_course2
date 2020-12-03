4fe49675005a264ac80378082de3232d857cd413
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-12-03 22:44:52 +0200

    Create Plot1.R

---
+++ Lab6/Plot1.R
@@ -0,0 +1,8 @@
+
+#### code for the 1st graphs
+
+hist(data_sample$Global_active_power, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", main = "Global Active Power")
+     
