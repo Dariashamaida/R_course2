b8975975f7e531243fb8183ae336428227f970ec
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-12-14 13:38:10 +0200

    Update Plot3.R

--- Lab6/Plot3.R
+++ Lab6/Plot3.R
@@ -3,3 +3,4 @@ plot(data_sample$DateTime, data_sample$Sub_metering_1, type = "l", xlab = "",  y
   lines(data_sample$DateTime, data_sample$Sub_metering_2, type = "l", col = "red")
   lines(data_sample$DateTime, data_sample$Sub_metering_3, type = "l", col = "blue")
   legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1 ", "Sub_metering_2 ", "Sub_metering_3 "))
+dev.off()
