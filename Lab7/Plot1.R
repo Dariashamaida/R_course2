7df6364f103fe3d075f2bcf9ebd684519e72a4d3
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-12-16 00:45:45 +0200

    Update Plot1.R

--- Lab7/Plot1.R
+++ Lab7/Plot1.R
@@ -23,6 +23,12 @@ totalYearEmission
 
 #renaming variable
 totalYearEmission<-rename(totalYearEmission,Year=Group.1)
+ totalYearEmission
+  Year       x    Total
+1 1999 7332967 7332.967
+2 2002 5635780 5635.780
+3 2005 5454703 5454.703
+4 2008 3464206 3464.206
 
 #creating plot
 plot(totalYearEmission$Year,totalYearEmission$Total, type='b', xlab='Year', ylab='Total Emission of PM2.5(tonns)')
