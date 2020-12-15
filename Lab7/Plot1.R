01b008ade9e8f879e23e5e71744144b0061a8ca3
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-12-16 00:12:42 +0200

    Create Plot1.R

---
+++ Lab7/Plot1.R
@@ -0,0 +1,9 @@
+
+#aggrigating data
+totalYearEmission <- aggregate(x=NEI$Emissions,by=list(NEI$year),FUN=sum)
+totalYearEmission 
+  Group.1       x
+1    1999 7332967
+2    2002 5635780
+3    2005 5454703
+4    2008 3464206
