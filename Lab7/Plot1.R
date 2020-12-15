55cd3b6281072b1769a8131ddefd11553a836595
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-12-16 00:44:49 +0200

    Update Plot1.R

--- Lab7/Plot1.R
+++ Lab7/Plot1.R
@@ -11,3 +11,20 @@ totalYearEmission
 2    2002 5635780
 3    2005 5454703
 4    2008 3464206
+
+#adding new variable
+totalYearEmission <-mutate(totalYearEmission,Total=x/1000) 
+totalYearEmission 
+  Group.1       x    Total
+1    1999 7332967 7332.967
+2    2002 5635780 5635.780
+3    2005 5454703 5454.703
+4    2008 3464206 3464.206
+
+#renaming variable
+totalYearEmission<-rename(totalYearEmission,Year=Group.1)
+
+#creating plot
+plot(totalYearEmission$Year,totalYearEmission$Total, type='b', xlab='Year', ylab='Total Emission of PM2.5(tonns)')
+
+#Results - As the obtained plot shows, total PM2.5 emissions in the United States have a tendency for decreasing from 1999 to 2008
