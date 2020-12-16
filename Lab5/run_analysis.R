8604ea8de450cdc2909efcdf2b2bb9d6d1a87411
Dariashamaida <61041300+Dariashamaida@users.noreply.github.com>
2020-12-16 12:43:44 +0200

    Update run_analysis.R

--- Lab5/run_analysis.R
+++ Lab5/run_analysis.R
@@ -1,5 +1,15 @@
-#reading test and train data and combining 2 datasets
-train_data <- read.table("G://KNU//6 курс//R//Lab 5//UCI HAR Dataset//train//X_train.txt")
-test_data <- read.table("G://KNU//6 курс//R//Lab 5//UCI HAR Dataset//test//X_test.txt")
-all_data <- rbind.data.frame(train_data, test_data)
+#creating dataframe with col names
+features_ <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\features.txt', colClasses = "character")[,2]
+
+#reading data
+
+#train
+x_train <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\train\\X_train.txt', col.names = features_, check.names = FALSE)
+y_train <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\train\\y_train.txt', col.names = c('Activity'))
+subject_train <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\train\\subject_train.txt', col.names = c('Subject'))
+
+#test
+x_test <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\test\\X_test.txt', col.names = features_, check.names = FALSE)
+y_test <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\test\\y_test.txt', col.names = c('Activity'))
+subject_test <- read.table('G:\\KNU\\6 курс\\R\\Lab 5\\UCI HAR Dataset\\test\\subject_test.txt', col.names = c('Subject'))
 
